b044f88adfd7471018db34c7e7dce60fdfdb403e
Sam Lawson <samilawson17@gmail.com>
2019-02-16 16:25:27 -0600

    admin and private routing

---
+++ frontend/src/components/Routes/AdminRoute.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cookie from 'js-cookie'
+import { Route, Redirect } from 'react-router-dom'
+
+const { id, admin } = cookie.get()
+
+const AdminRoute = ({ component, ...rest }) => (
+    <Route
+        {...rest}
+        render={(props) => {
+            if(!id){
+                cookie.set('target', props.location.pathname)
+            }
+            return (
+                admin ? (
+                    React.createElement(component, props)
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: id ? '/unauthorized' : '/login',
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+            )
+        }}
+    />
+)
+
+AdminRoute.propTypes = {
+    component: PropTypes.func,
+    location: PropTypes.shape({
+      hash: PropTypes.string,
+      key: PropTypes.string,
+      pathname: PropTypes.string,
+      search: PropTypes.string,
+      state: PropTypes.string,
+    }),
+  };
+  
+  AdminRoute.defaultProps = {
+    component: null,
+    location: {
+      hash: '',
+      key: '',
+      pathname: '',
+      search: '',
+      state: '',
+    },
+  };
+  
+  export default AdminRoute;
