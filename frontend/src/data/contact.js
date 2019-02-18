1732d4e2da281830f93d56033617cf4052a18099
Sam Lawson <samilawson17@gmail.com>
2019-02-18 08:22:39 -0600

    add data, remove unneeded files

---
+++ frontend/src/data/contact.js
@@ -0,0 +1,42 @@
+import faGithub from "@fontawesome/fontawesome-free-brands/faGithub";
+import faFacebook from "@fontawesome/fontawesome-free-brands/faFacebookF";
+import faInstagram from "@fontawesome/fontawesome-free-brands/faInstagram";
+import faLinkedinIn from "@fontawesome/fontawesome-free-brands/faLinkedinIn";
+import faTwitter from "@fontawesome/fontawesome-free-brands/faTwitter";
+import faKaggle from "@fontawesome/fontawesome-free-brands/faKaggle";
+import faEnvelope from "@fontawesome/fontawesome-free-brands/faEnvelope";
+
+const data = [
+  {
+    link: "https://github.com/samilawson",
+    label: "Github",
+    icon: faGithub
+  },
+  {
+    link: "https://www.facebook.com/profile.php?id=100010509188454",
+    label: "Facebook",
+    icon: faFacebook
+  },
+  {
+    link: "https://www.instagram.com/sam.lawson17",
+    label: "Instagram",
+    icon: faInstagram
+  },
+  {
+    link: "https://www.linkedin.com/in/sam-lawson-75899a140/",
+    label: "LinkedIn",
+    icon: faLinkedinIn
+  },
+  {
+    link: "https://twitter.com/slawsonfc",
+    label: "Twitter",
+    icon: faTwitter
+  },
+  {
+    link: "mailto:samilawson17@gmail.com",
+    label: "Email",
+    icon: faEnvelope
+  }
+];
+
+export default data;
